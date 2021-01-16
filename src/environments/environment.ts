82fdb5eaeb58b11f5ddbaa12de7168e975e662aa
lfreddyl <orlanjack95@gmail.com>
2021-01-15 19:57:59 -0500

    deploy finalv3

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   wsUrl: 'https://backendreciclamos.herokuapp.com/',
   apiUrl:'https://backendreciclamos.herokuapp.com/',
-  urlStorage:'http://ec2-15-228-77-70.sa-east-1.compute.amazonaws.com:4000/'
+  urlStorage:'15.228.77.70:4000/'
 };
 
 /*
